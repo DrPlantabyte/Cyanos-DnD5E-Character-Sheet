263561eeff37e6c4c7ce20f1405eced24ea18dbf
Cyanobacterium <github.com/cyanobacteruim>
2016-10-19 17:04:51 -0400

    improved build scripts

--- build.sh
+++ build.sh
@@ -12,4 +12,9 @@ $inkscape Pg1-B.svg --export-area-page --export-text-to-path --export-pdf dist/p
 $inkscape Pg2-A.svg --export-area-page --export-text-to-path --export-pdf dist/pg2A.pdf
 $inkscape Pg2-B.svg --export-area-page --export-text-to-path --export-pdf dist/pg2B.pdf
 
+$inkscape Pg1-A.svg --export-area-page --export-background "white" --export-dpi 40 --export-png dist/preview_pg1A.png
+$inkscape Pg1-B.svg --export-area-page --export-background "white" --export-dpi 40 --export-png dist/preview_pg1B.png
+$inkscape Pg2-A.svg --export-area-page --export-background "white" --export-dpi 40 --export-png dist/preview_pg2A.png
+$inkscape Pg2-B.svg --export-area-page --export-background "white" --export-dpi 40 --export-png dist/preview_pg2B.png
+
 $pdftk dist/pg1A.pdf dist/pg1B.pdf dist/pg2A.pdf dist/pg2B.pdf cat output dist/DrCyanos_DnD-Character-Sheet.pdf
